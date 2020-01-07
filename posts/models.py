2875e1f08e9273338feb78ed0f78586e3f3124a5
kmurata798 <kmurata798@gmail.com>
2020-01-06 17:00:10 -0800

    added models/views/urls

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
-# Create your models here.
+
+class Title(models.Model):
+    title_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.title_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Body(models.Model):
+    question = models.ForeignKey(Title, on_delete=models.CASCADE)
+    body_text = models.CharField(max_length=500)
+    likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.body_text
