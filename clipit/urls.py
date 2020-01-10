5e615f9975452471e2f2fd6fb44c7ac0258eb7fb
kmurata798 <kmurata798@gmail.com>
2020-01-10 12:18:57 -0800

    delete post app

--- clipit/urls.py
+++ clipit/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('blog.urls')),
-    path('posts/', include('posts.urls')),
+    # path('posts/', include('posts.urls')),
 ]
 
 if settings.DEBUG:
